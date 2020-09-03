d2fe1615aa8ca7337b5d39c6328f7aaf1cf2d893
chuangmaster <chuang.master@gmail.com>
2020-09-04 00:56:23 +0800

    pass value from parent to child

--- ComponentCommunication/src/components/User.vue
+++ ComponentCommunication/src/components/User.vue
@@ -3,11 +3,15 @@
     <h1>The User Component</h1>
     <p>I'm an awesome User!</p>
     <button @click="changeName()">change my name</button>
+    <p>My name is {{ name }}</p>
     <hr />
     <div class="row">
       <div class="col-xs-12 col-sm-6">
         <!-- <app-user-detail v-bind:name="name"></app-user-detail> -->
-        <app-user-detail :name="name"></app-user-detail>
+        <app-user-detail
+          :name="name"
+          @resetName="name = $event"
+        ></app-user-detail>
       </div>
       <div class="col-xs-12 col-sm-6">
         <app-user-edit></app-user-edit>
@@ -21,19 +25,23 @@ import UserDetail from "./UserDetail.vue";
 import UserEdit from "./UserEdit.vue";
 
 export default {
-  data: function() {
+  data: function () {
     return { name: "Max" };
   },
   methods: {
     changeName() {
       let that = this;
-      this.name = "Xavier";
-    }
+      that.name = "Xavier";
+    },
+    resetName(name) {
+      let that = this;
+      that.name = name;
+    },
   },
   components: {
     appUserDetail: UserDetail,
-    appUserEdit: UserEdit
-  }
+    appUserEdit: UserEdit,
+  },
 };
 </script>
 
--- ComponentCommunication/src/components/UserDetail.vue
+++ ComponentCommunication/src/components/UserDetail.vue
@@ -2,13 +2,25 @@
   <div class="component">
     <h3>You may view the User Details here</h3>
     <p>Many Details</p>
-    <p>User Name: {{name}}</p>
+    <p>User Name: {{ exchangeName() }}</p>
+    <button @click="reset">reset</button>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["name"]
+  props: ["name"],
+  methods: {
+    exchangeName() {
+      let that = this;
+      return that.name.split("").reverse().join("");
+    },
+    reset() {
+      let that = this;
+      that.name = "Max";
+      that.$emit("resetName", that.name);
+    },
+  },
 };
 </script>
 
