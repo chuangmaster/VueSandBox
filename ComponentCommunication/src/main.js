f8af3bd68c2722a6fc8e10b97176cddfd88fd187
chuangmaster <chuang.master@gmail.com>
2020-09-06 22:50:57 +0800

    init

--- ComponentCommunication/src/main.js
+++ ComponentCommunication/src/main.js
@@ -1,7 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 
-export const eventBus = new Vue();
+export const eventBus = new Vue({
+  methods: {
+    editAge(age) {
+      this.$emit("wasEditedAge", age);
+    },
+  },
+});
 new Vue({
   el: "#app",
   render: (h) => h(App),
---
+++ SimpleModule/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <appQuote></appQuote>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from "./components/Quotes.vue";
+export default {
+  components: {
+    appQuote: Quote,
+  },
+};
+</script>
+
+<style></style>
